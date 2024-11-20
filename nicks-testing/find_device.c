6082509a91fb1e461e2ac4126c01685661187028
Nicholas Katsaros <nicholaskatsaros@nevada.unr.edu>
2024-11-20 13:53:12 -0800

    Added ability to find network device

--- nicks-testing/find_device.c
+++ nicks-testing/find_device.c
@@ -1,3 +1,5 @@
+// Code from Pawel Sosnowski on https://www.devdungeon.com/content/using-libpcap-c
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <pcap.h>
@@ -8,6 +10,10 @@ char ebuf[PCAP_ERRBUF_SIZE];
 pcap_if_t *devs;
 
 pcap_findalldevs(&devs, ebuf);
+
+for (int i = 0; devs[i] != NULL; i++){
+    printf("%s", devs[i]->name);
+}
 
 printf("%s\n", devs->name);
 
