e5a11ebb00e0d1acd4c920c32db7f05151185ce2
Nicholas Katsaros <nicholaskatsaros@nevada.unr.edu>
2024-12-08 01:13:03 -0800

    added automated compilation of c dependencies

--- nicks-testing/capture_starter.py
+++ nicks-testing/capture_starter.py
@@ -14,7 +14,12 @@ def read_until_input_needed(process):
     print("Escaped loop")
     return output
 
-if __name__ == "__main__":
+
+def get_net_device():
+    compilation_command = ["gcc", "-o", "find_device", "find_device.c", "-lpcap"]
+
+    subprocess.run(compilation_command, text=True)
+    
     device_finder_path = "./find_device"
 
     process = subprocess.Popen(
@@ -34,21 +39,53 @@ if __name__ == "__main__":
     process.stdin.flush()
 
     line = process.stdout.readline()
+    device_name = line.strip()
     print(line.strip())
 
-    line = process.stdout.readline()
-    print(line.strip())
-
-    line = process.stdout.readline()
-    print(line.strip())
-
-    line = process.stdout.readline()
-    print(line.strip())
+    for i in range(3):
+        line = process.stdout.readline()
+        print(line.strip())
 
     exit_code = process.wait()
 
     print("Exit code: " + str(exit_code))
+    if(exit_code == 0):
+        print("Process terminated succesfully")
+    else:
+        print("Error encountered. Process is still running")
+        print("Process is being forcibly terminated")
+        process.terminate()
+        process.wait()
+
+    return device_name
+
+
+if __name__ == "__main__":
+    #device = get_net_device()
+    device = "eth0"
+
+    compilation_command = ["gcc", "-o", "capture_packets", "capture_packets.c", "-lpcap"]
+
+    subprocess.run(compilation_command, text=True)
+
+    packet_capture_command = ["sudo", "./capture_packets"]
+
+    process = subprocess.Popen(
+        packet_capture_command,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True 
+    )
 
 
 
+    
+
+    
+
+
+    
+
+    
     
